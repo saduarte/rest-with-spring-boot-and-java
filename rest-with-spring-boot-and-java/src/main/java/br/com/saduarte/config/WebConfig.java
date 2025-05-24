bd337607f169d9c5a94e04ef0ab3751a9706a25b
Sabrina Duarte <sabrina.md@outlook.com>
2025-05-23 22:19:03 -0300

    feat: implementa Contet Negotiation

---
+++ rest-with-spring-boot-and-java/src/main/java/br/com/saduarte/config/WebConfig.java
@@ -0,0 +1,23 @@
+package br.com.saduarte.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+        // via EXTENSION. http://localhost:8080/api/person/v1.xml  or  http://localhost:8080/api/person/v1.JSON Deprecated on Spring Boot 2.6
+
+        // Via QUERY PARA http://localhost:8080/api/person/v1/2?mediaType=xml
+        configurer.favorParameter(true).
+                parameterName("mediaType").
+                ignoreAcceptHeader(true).useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                    .mediaType("json", MediaType.APPLICATION_JSON)
+                    .mediaType("json", MediaType.APPLICATION_XML);
+    }
+}
--- rest-with-spring-boot-and-java/src/main/java/br/com/saduarte/data/dto/PersonDTO.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/saduarte/data/dto/PersonDTO.java
@@ -13,15 +13,6 @@ public class PersonDTO implements Serializable {
     private String address;
     private String gender;
 
-    public PersonDTO() {}
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
 
     public Long getId() {
         return id;
@@ -29,6 +20,14 @@ public class PersonDTO implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
@@ -39,14 +38,6 @@ public class PersonDTO implements Serializable {
         this.lastName = lastName;
     }
 
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
     public String getAddress() {
         return address;
     }
@@ -55,15 +46,25 @@ public class PersonDTO implements Serializable {
         this.address = address;
     }
 
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public PersonDTO() {}
+
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof PersonDTO person)) return false;
-        return Objects.equals(getId(), person.getId()) && Objects.equals(getFirstName(), person.getFirstName()) && Objects.equals(getLastName(), person.getLastName()) && Objects.equals(getAddress(), person.getAddress()) && Objects.equals(getGender(), person.getGender());
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonDTO personDTO = (PersonDTO) o;
+        return Objects.equals(getId(), personDTO.getId()) && Objects.equals(getFirstName(), personDTO.getFirstName()) && Objects.equals(getLastName(), personDTO.getLastName()) && Objects.equals(getAddress(), personDTO.getAddress()) && Objects.equals(getGender(), personDTO.getGender());
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(getId(), getFirstName(), getLastName(), getAddress(), getGender());
     }
-
 }
