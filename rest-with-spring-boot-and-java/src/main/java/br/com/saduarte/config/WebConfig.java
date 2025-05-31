351a546188a4539c2393cbe6121a672786781930
Sabrina Duarte <sabrina.md@outlook.com>
2025-05-31 12:13:08 -0300

    feat: implementa o uso de CORS globalmente no arquivo YAML

--- rest-with-spring-boot-and-java/src/main/java/br/com/saduarte/config/WebConfig.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/saduarte/config/WebConfig.java
@@ -1,34 +1,38 @@
 package br.com.saduarte.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatters= "";
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatters.split(",");
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+               // .allowedMethods("GET","POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
-        // via EXTENSION. http://localhost:8080/api/person/v1.xml  or  http://localhost:8080/api/person/v1.JSON Deprecated on Spring Boot 2.6
-
-        /**
-        // Via QUERY PARA http://localhost:8080/api/person/v1/2?mediaType=xml
-        configurer.favorParameter(true).
-                parameterName("mediaType").
-                ignoreAcceptHeader(true).useRegisteredExtensionsOnly(false)
-                .defaultContentType(MediaType.APPLICATION_JSON)
-                    .mediaType("json", MediaType.APPLICATION_JSON)
-                    .mediaType("json", MediaType.APPLICATION_XML);
-    }
-         **/
-
-        // Via QUERY PARA http://localhost:8080/api/person/v1/2?mediaType=xml
         configurer.favorParameter(false).
                 ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                     .mediaType("json", MediaType.APPLICATION_JSON)
-                    .mediaType("json", MediaType.APPLICATION_XML);
+                    .mediaType("json", MediaType.APPLICATION_XML)
+                    .mediaType("yaml", MediaType.APPLICATION_YAML);
     }
 }
